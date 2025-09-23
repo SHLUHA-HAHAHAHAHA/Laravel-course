fc376c65100f0ce220b74c8cf006c450fe8257ea
Размыслов IS 41 <zalupa@ml.com>
2025-09-23 14:44:32 +0300

    Урок 1-6

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\MyPlaceController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class,'index']);
